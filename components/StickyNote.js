bb8bdd625eb47bc8683c77f71dfde2df025224fb
AlainAmbrose <ambrosealain@gmail.com>
2025-02-02 23:54:09 -0500

    made header transparent

--- components/StickyNote.js
+++ components/StickyNote.js
@@ -1,6 +1,6 @@
 const StickyNote = ({ title, content }) => {
   return (
-    <div className="bg-zinc-600 h-max min-w-full min-h-[50vh] rounded-lg shadow mb-5 p-4 break-words hover:shadow-xl hover:shadow-zinc-700 transition-shadow">
+    <div className="bg-rose-600 h-max min-w-full min-h-[50vh] rounded-lg shadow mb-5 p-4 break-words hover:shadow-xl hover:shadow-zinc-700 transition-shadow">
       <h2 className="text-lg text-white font-semibold mb-1">{title}</h2>
       <p className="text-white">{content}</p>
     </div>
